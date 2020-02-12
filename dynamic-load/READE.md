ce64d67729ce29e8ba0240e95617487cdf89fe1c
jiayuxin <13520723393@163.com>
2020-02-12 21:31:33 +0800

    support parse json string in etcd

--- dynamic-load/READE.md
+++ dynamic-load/READE.md
@@ -2,4 +2,8 @@
 
 ## confd+nginx
 
+### confd
+
+[读取 json](https://github.com/kelseyhightower/confd/blob/master/docs/templates.md#complex-example)
+
 ### docker process manager
--- dynamic-load/etcd_set.sh
+++ dynamic-load/etcd_set.sh
@@ -1,6 +1,6 @@
 etcdctl put /myapp/subdomain myapp
-etcdctl put /myapp/upstream/app2 "10.0.1.100:80"
-etcdctl put /myapp/upstream/app1 "10.0.1.101:80"
+etcdctl put /myapp/upstream/app2 '{"IP": "100.0.0.2","Port":8080}'
+etcdctl put /myapp/upstream/app1 '{"IP": "100.0.0.1","Port":8080}'
 etcdctl put /yourapp/subdomain yourapp
-etcdctl put /yourapp/upstream/app2 "10.0.1.102:80"
-etcdctl put /yourapp/upstream/app1 "10.0.1.103:80"
+etcdctl put /yourapp/upstream/app2 '{"IP": "101.0.0.2","Port":8080}'
+etcdctl put /yourapp/upstream/app1 '{"IP": "101.0.0.1","Port":8080}'
