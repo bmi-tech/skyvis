f5a466c681226924daba3f33b05c6fa94399a608
jiayuxin <13520723393@163.com>
2020-02-18 23:06:18 +0800

    save

--- dynamic-load/etcd_set.sh
+++ dynamic-load/etcd_set.sh
@@ -1,4 +1,4 @@
-etcdctl put /services/web/cust1/2 '{"IP": "localhost","Port":8080}'
+etcdctl put /services/web/hls/2 '{"IP": "localhost","Port":8080}'
 # etcdctl put /services/web/cust2/2 '{"IP": "localhost","Port":8080}'
 # etcdctl put /services/web/cust2/1 '{"IP": "localhost","Port":8080}'
-etcdctl put /services/web/cust1/1 '{"IP": "localhost","Port":8080}'
+etcdctl put /services/web/gateway/1 '{"IP": "localhost","Port":8080}'
---
+++ dynamic-load/start_etcd.sh
@@ -0,0 +1,1 @@
+etcd  --listen-client-urls 'http://0.0.0.0:2379' --advertise-client-urls 'http://0.0.0.0:2379'
